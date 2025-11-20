4c30637904958849a22e0f9519c22a05ebc3d1fe
James McMichael <tur08816@temple.edu>
2025-11-20 14:50:11 -0500

    all tests pass for UniqueCollection.kt

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -7,23 +7,37 @@ class UniqueCollectionTest {
 
     lateinit var collection: UniqueCollection
 
+    lateinit var Item1: Item
+    lateinit var Item2: Item
+    lateinit var string1: String
+
     @Before
     fun setUp() {
         collection = UniqueCollection()
+        string1 = "Item1"
+        Item1 = Item(string1)
+        Item2 = Item("two")
     }
 
 
     // TODO 1: Write a test to ensure items can be added to the collection
     @Test
     fun addAnItem() {
-
+        collection.addItem(Item1)
+        val Item = collection.get(0)
+        assert(Item.name == Item1.name)
     }
 
     // TODO 2: Write a test to ensure that only unique items can be added to the collection
     // Uniqueness is determined by the Item.name property, which is set via the constructor
     @Test
     fun addUniqueItem() {
-
+        collection.addItem(Item1)
+        collection.addItem(Item1)
+        collection.addItem(Item(string1))
+        collection.addItem(Item2)
+        val numberOfItemsInCollection = collection.size()
+        assert(numberOfItemsInCollection == 2)
     }
 
     // Test Driven Development (TDD) test - complete specified function so that this test passes
